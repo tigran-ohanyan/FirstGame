6409eaa1a5887fd2cd4f67b988e22589e241fafc
tigran-ohanyan <copklient13@gmail.com>
2024-06-05 22:51:55 +0400

    send for coding FPS controller

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    int leftFingerId, rightFingerId;
+    float halfScreenWidth;
+
+    void Start(){
+        leftFingerId = -1;
+        rightFingerId = -1;
+
+    }
+}
